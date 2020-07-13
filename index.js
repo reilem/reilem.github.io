e0d2be24acdf810053cde204aaa0dbc480776d16
Reinert L <reilemx@gmail.com>
2020-07-13 18:17:37 +0200

    Implement links

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function scrollToSecond () {
         behavior: "smooth",
     })
 }
+
+function goToIndex () {
+    window.location = "index.html"
+}
+
+function goToContact () {
+    window.location = "contact.html"
+}
+
+function openLinkedIn () {
+    window.open("https://www.linkedin.com/in/reinert-lemmens/")
+}
+
+function openGithub () {
+    window.open("https://github.com/reilem")
+}
