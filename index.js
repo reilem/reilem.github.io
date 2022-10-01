c4187cba8388717032a52711fe541e9bc023cd9e
Reinert Lemmens <reilemx@gmail.com>
2022-10-02 00:16:51 +0200

    Fix header + Make mesh darker & more sparse

--- index.js
+++ index.js
@@ -6,8 +6,10 @@
  * - More content, timeline, etc.
  */
 const POINT_COUNT = 200;
-const LINE_COLOR = '#5c5c5c';
+const LINE_COLOR = '#464646';
 const MESH_MARGIN = 100;
+const SEED = '223357780';
+const MIN_TRIANGLE_SIZE = 30;
 
 function scrollToSecond() {
     window.scroll({
@@ -48,9 +50,30 @@ function makeArray(size) {
  * @returns {{x: number, y: number}[]}
  */
 function generateMeshPoints() {
-    const myrng = new Math.seedrandom('223456780');
+    const myrng = new Math.seedrandom(SEED);
     const margin = MESH_MARGIN;
-    return generatePoints(myrng, margin, margin, window.innerWidth - margin * 2, window.innerHeight - margin * 2, POINT_COUNT);
+    const points = generatePoints(myrng, margin, margin, window.innerWidth - margin * 2, window.innerHeight - margin * 2, POINT_COUNT);
+    return removePointsTooCloseTogether(points);
+}
+
+/**
+ * @param {{x: number, y: number}[]} points
+ * @returns {{x: number, y: number}[]}
+ */
+function removePointsTooCloseTogether(points) {
+    const sortedPoints = [...points].sort((p1, p2) => p1.x - p2.x);
+    return sortedPoints.filter((point, index) => {
+        return !sortedPoints.slice(index + 1, index + 10).some(point2 => distance(point, point2) < MIN_TRIANGLE_SIZE);
+    });
+}
+
+/**
+ * @param {{x: number, y: number}} p1
+ * @param {{x: number, y: number}} p2
+ * @returns {number}
+ */
+function distance(p1, p2) {
+    return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);
 }
 
 /**
@@ -90,6 +113,23 @@ function drawTriangles(ctx, points, delaunay) {
 }
 
 /**
+ *
+ * @param {{{x: number, y: number, index: number}[]} points} points
+ */
+function isTriangleTooThin(points) {
+    const xCoords = points.map(({ x }) => x);
+    const yCoords = points.map(({ y }) => y);
+    const yMin = Math.min(...yCoords);
+    const yMax = Math.max(...yCoords);
+    const xMin = Math.min(...xCoords);
+    const xMax = Math.max(...xCoords);
+    if (xMax - xMin < MIN_TRIANGLE_SIZE || yMax - yMin < MIN_TRIANGLE_SIZE) {
+        return true;
+    }
+    return false;
+}
+
+/**
  * @param {CanvasRenderingContext2D} ctx
  * @param {{x: number, y: number}[]} points
  * @param {{ triangles: number[], hull: number[] }} delaunay
@@ -102,13 +142,14 @@ function drawTriangle(ctx, points, delaunay, index) {
         const pointIndex = triangles[i + offset];
         return { ...points[pointIndex], index: pointIndex };
     });
+    const triangleIsOnHull = trianglePoints.some(({ index }) => hull.includes(index));
+    if (triangleIsOnHull || isTriangleTooThin(trianglePoints)) {
+        return;
+    }
     trianglePoints.forEach((point, i) => {
         const isLastPoint = i === trianglePoints.length - 1;
         const nextPoint = isLastPoint ? trianglePoints[0] : trianglePoints[i + 1];
-        const aPointOnHull = hull.includes(point.index) || hull.includes(nextPoint.index);
-        if (!aPointOnHull) {
-            drawLine(ctx, point, nextPoint);
-        }
+        drawLine(ctx, point, nextPoint);
     });
 }
 
@@ -127,6 +168,9 @@ function drawLine(ctx, { x: x0, y: y0 }, { x: x1, y: y1 }) {
 }
 
 function draw() {
+    if (window.innerWidth < 650) {
+        return;
+    }
     const ctx = initialiseCanvas();
     const points = generateMeshPoints();
     const delaunay = new Delaunator(pointsToArray(points));
